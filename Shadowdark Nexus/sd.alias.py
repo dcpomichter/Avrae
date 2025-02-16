31ba82886e18a5a44cc97a2a8be2833f46cbffe6
dcpomichter <dcpomichter@gmail.com>
2025-02-15 23:41:29 -0500

    Added exhaustion Disadvantage to alias

--- Shadowdark Nexus/sd.alias.py
+++ Shadowdark Nexus/sd.alias.py
@@ -1,6 +1,5 @@
 embed <drac2>
-ch=character()
-arg=&ARGS&
+c, ch, arg=combat(), character(), &ARGS&
 args=argparse(arg)
 stats=['strength','dexterity','constitution','intelligence','wisdom','charisma']
 checks=['melee','ranged','spellcasting']
@@ -10,17 +9,30 @@ intCaster=['wizard']
 chaCaster=['bard','knight of st ydris','witch',]
 adv=args.adv()
 misc=args.last('b',0,int)
-DC=args.last('dc',0,int)
+checkDc=args.last('dc',0,int)
 mastery=args.get('mastery')
 if mastery:
     misc+=1+ch.levels.total_level//2
 stat=''
 props=' '
 bonus=0
+if c:
+    cb=c.get_combatant(ch.name)
+else:
+    cb=ch.name
+exhaustedStats=[]
+if c:
+    for option in stats:
+        if cb.get_effect(f'Exhaustion ({option.title()})'):
+            exhaustedStats.append(option)
 if arg:
     for stat in stats:
         if arg[0].lower() in stat:
             bonus=ch.stats.get_mod(stat)
+            for options in exhaustedStats:
+                if stat in options:
+                    if adv!=-1:
+                        adv-=1
             stat=stat.title()
             break
         else:
@@ -35,6 +47,22 @@ if arg:
                         break
                     else:
                         bonus=ch.stats.get_mod(stats[0])
+                if props!=' ':
+                    if ch.stats.get_mod(stats[1])>ch.stats.get_mod(stats[0]):
+                            for options in exhaustedStats:
+                                if stats[1] in options:
+                                    if adv!=-1:
+                                        adv-=1
+                    else:
+                        for options in exhaustedStats:
+                            if stats[0] in options:
+                                if adv!=-1:
+                                    adv-=1
+                else:
+                    for options in exhaustedStats:
+                        if stats[0] in options:
+                            if adv!=-1:
+                                adv-=1
                 talents=int(ch.get_cvar('meleeCheck', 0))
                 bonus+=talents
                 stat=check.title()
@@ -47,6 +75,22 @@ if arg:
                         break
                     else:
                         bonus=ch.stats.get_mod(stats[1])
+                if props!=' ':
+                    if ch.stats.get_mod(stats[0])>ch.stats.get_mod(stats[1]):
+                            for options in exhaustedStats:
+                                if stats[0] in options:
+                                    if adv!=-1:
+                                        adv-=1
+                    else:
+                        for options in exhaustedStats:
+                            if stats[1] in options:
+                                if adv!=-1:
+                                    adv-=1
+                else:
+                    for options in exhaustedStats:
+                        if stats[1] in options:
+                            if adv!=-1:
+                                adv-=1
                 talents=int(ch.get_cvar('rangedCheck', 0))
                 bonus+=talents
                 stat=check.title()
@@ -55,16 +99,28 @@ if arg:
                 for caster in wisCaster:
                     if caster in ch.get_cvar('class').lower():
                         bonus=ch.stats.get_mod(stats[4])
+                        for options in exhaustedStats:
+                            if stats[4] in options:
+                                if adv!=-1:
+                                    adv-=1
                         props=f''' {caster.title()} '''
                         break
                 for caster in intCaster:
                     if caster in ch.get_cvar('class').lower():
                         bonus=ch.stats.get_mod(stats[3])
+                        for options in exhaustedStats:
+                            if stats[3] in options:
+                                if adv!=-1:
+                                    adv-=1
                         props=f''' {caster.title()} '''
                         break
                 for caster in chaCaster:
                     if caster in ch.get_cvar('class').lower():
                         bonus=ch.get_mod(stats[5])
+                        for options in exhaustedStats:
+                            if stats[5] in options:
+                                if adv!=-1:
+                                    adv-=1
                         props=f''' {caster.title()} '''
                         break
                 talents=int(ch.get_cvar('spellCheck', 0))
@@ -76,9 +132,9 @@ dice=['1d20','2d20kh1','2d20kl1'][adv]
 check=vroll(f'''{dice}+{bonus}''')
 difficulty=''
 text=f''' -title "{name} makes a{props}{stat} check!" -f "{check}" '''
-if DC>0:
-    difficulty=f''' -f "DC|{DC}" '''
-    if check.total>=DC:
+if checkDc>0:
+    difficulty=f''' -f "DC|{checkDc}" '''
+    if check.total>=checkDc:
         text=f''' -title "{name} makes a{props}{stat} check!" {difficulty} -f "{check} **(Success)**" '''
     else:
         text=f''' -title "{name} makes a{props}{stat} check!" {difficulty} -f "{check} **(Failure)**" '''
